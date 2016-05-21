285080aa935fa4555373f3f7c2cd08b6a7c3298d
atchandj <atchandjou@gmail.com>
2016-05-21 15:14:50 +0200

    Put script.js in folder js

--- WebContent/js/script.js
+++ WebContent/js/script.js
@@ -2,4 +2,12 @@ $(document).ready(function() {
     $(".clickable-row").click(function() {
         window.document.location = $(this).data("href");
     });
+    
+    
+	if($('table').length > 0){
+		for(var i = 0, max = $('table').length ; i < max; i++){
+			if($('table').eq(i).hasClass("dataTable"))
+			$('table').eq(i).DataTable();
+		}
+	}
 });
--- WebContent/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-	if($('form input').length != 0){
-		$('form input')[0].checked =  true ;
-	}
-	else{
-		$('#dataTable').DataTable();
-	}
-    
-} );
